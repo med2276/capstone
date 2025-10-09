6e200a21f8eee82f4cba58cd7b4468a6f5812766
med2276 <med2276@rit.edu>
2025-10-09 01:39:17 -0400

    Randomly select a prompt from the CSV

--- prompt_selection.py
+++ prompt_selection.py
@@ -6,20 +6,40 @@ to specify it meets certain criteria.
 FILEPATH = "prompt_database.csv"
 
 # Imports
+import random
 import pandas as pd
 
-def openCsv():
+def randomPromptReview():
     """
-    Opens a CSV file and returns its contents as a list of lists.
+    Randomly selects a prompt and allows user to review it.
     """
-    df = pd.read_csv(FILEPATH, usecols=["id", "en"], index_col="id")
+    # open the prompt database CSV
+    data = pd.read_csv(FILEPATH, usecols=["id", "en", "tags", "pulled","approved","comments","intials"], index_col="id")
+
+    # filter to only the unpulled data
+    unpulled = data["pulled"].str.lower() == "no"
+    
+    # check if there are any unpulled prompts
+    if unpulled.empty:
+        print("All prompts have been pulled!!")
+    
+    else:
+        # randonly select an ID from the remaining unpulled prompts
+        randomID = random.choice(data[unpulled].index)
+        selectedPrompt = data.loc[randomID]
+
+        # Print the prompt details
+        print("\n --- Review Prompt --- \n")
+        print(f"ID:\t {randomID}")
+        print(f"TAGS:\t {selectedPrompt['tags']}")
+        print(f"PROMPT:\t {selectedPrompt['en']}")
 
 def main ():
     """
     Main function to run the prompt selector.
     """
     print("Welcome to the prompt selector!")
-    openCsv()
+    randomPromptReview()
 
 if __name__ == "__main__":
     main()
